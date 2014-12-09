4d6a855ab1ad052b2507b6b7dc75a7f59651bdb7
Stephanie Hyland <steph.hyland@gmail.com>
2014-12-09 14:59:22 -0500

    includes CDF, using scipy to generate laplace-distributed RVs now

--- vis/plot_laplace.py
+++ vis/plot_laplace.py
@@ -2,6 +2,7 @@
 # Plot a laplace distribution with specified parameters.
 
 import numpy as np
+from scipy.stats import laplace
 import matplotlib.pyplot as plt
 import sys
 
@@ -13,9 +14,11 @@ b = int(sys.argv[2])
 
 # generate samples
 # lots of samples are a lazy-man's smoothing
-lap_X = np.random.laplace(loc=mu, scale=b, size=1000000)
+lap_X = laplace.rvs(loc=mu, scale=b, size=1000000)
+x = np.linspace(start=-8*b,stop=8*b,num=1000000)
+lap_Q = laplace.cdf(x=x, loc=mu, scale=b)
 
-# plotting!
+# plotting pdf!
 fig = plt.figure()
 h = plt.hist(lap_X, bins=500, alpha=1.0, color=t_green, normed=True, histtype='stepfilled', antialiased=True, linewidth=0)
 # to make the plot symmetric, find maximum distance to centre
@@ -24,8 +27,21 @@ upper_dx= h[1][-1] - mu
 max_dx = np.max([abs(lower_dx), abs(upper_dx)])
 plt.xlim(-max_dx + mu, max_dx +mu)
 # labels etc.
-plt.title('Laplace distribution with mu = '+str(mu)+' and b = '+str(b), family='monospace', size=16)
+plt.title('Laplace PDF with mu = '+str(mu)+' and b = '+str(b), family='monospace', size=16)
 plt.xlabel('x', family='monospace', size=16)
 plt.ylabel('p(x)', family='monospace', size=16)
 plt.tight_layout()      # requires Agg
-plt.savefig('laplace_mu'+str(mu)+'_b'+str(b)+'.png', dpi=200)
+plt.savefig('laplacePDF_mu'+str(mu)+'_b'+str(b)+'.png', dpi=200)
+plt.clf()
+
+# plotting cdf!
+fig = plt.figure()
+#h = plt.plot(x, lap_Q, color=t_green, linestyle='solid')
+h = plt.fill_between(x, 1-lap_Q, 0, color=t_green, linestyle='solid')
+plt.title('Laplace CDF with mu = '+str(mu)+' and b = '+str(b), family='monospace', size=16)
+plt.xlabel('C', family='monospace', size=16)
+plt.ylabel('p(x > C)', family='monospace', size=16)
+plt.ylim(0,1.1)
+plt.xlim(-8*b,8*b)
+plt.savefig('laplaceCDF_mu'+str(mu)+'_b'+str(b)+'.png', dpi=200)
+plt.clf()
