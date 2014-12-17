b71aade051ab2469fd36301ba6cc080f67a72af9
Stephanie Hyland <steph.hyland@gmail.com>
2014-12-16 21:18:42 -0500

    few things: changed centres->components, added the option to 'rescale' (+recentre) each individual mixture to be between exp(-1) and exp(1) (before normalising globally)... this has the practical effect of preventing any one component from really dominating visually, but is obviously a hack and distorts the purity of the mixture pdf

--- vis/multinorm_mixture.py
+++ vis/multinorm_mixture.py
@@ -5,6 +5,12 @@
 # (for each component of the mixture)
 # a uniform prior (between -1 and +1) over means
 # a uniform prior over the *scale* of the covariance matrix, given a randomly-generated one.
+#
+# TODO
+# 1. Prettify scale on Z-axis.
+# 2. Experiment with other colour maps.
+# 3. Put a better prior on the covariance matrices.
+# 4. Better resolution/cropping on savefig.
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,13 +18,22 @@ from mpl_toolkits.mplot3d import Axes3D
 from scipy.stats import multivariate_normal
 import sys
 
+rescale = True
+if rescale:
+    # this is for forcing the unnormalised probabilities to fall between
+    # exp(-1) and exp(1)
+    good_min = np.exp(-1)
+    good_max = np.exp(1)
+    good_range = good_max - good_min
+
 if len(sys.argv) < 3:
     N = 1000
-    centres = N/50
-    print 'Using defaults: \nN =', N, '\ncentres =', centres
+    components = N/50
+    print 'Using defaults: \nN =', N, '\ncomponents =', components
 else:
     N = int(sys.argv[1])
-    centres = int(sys.argv[2])
+    components = int(sys.argv[2])
+filename = '3D_PDF.N'+str(N)+'.c'+str(components)+'.png'
 # make the mesh
 X = np.linspace(-1, 1, N)
 Y = np.linspace(-1, 1, N)
@@ -34,13 +49,21 @@ cov = np.dot(A,A.T)
 print 'Covariance matrix:'
 print cov
 # build mixture components
-pi = np.random.rand(centres)
+pi = np.random.rand(components)
 pi = pi/np.sum(pi)
 # sequentially build Z up
 Z = 0
-for n in xrange(centres):
+for n in xrange(components):
     mean = 1 - 2*np.random.rand(2)
     scale = np.random.rand(1)
+    Zn = multivariate_normal.pdf(pos, mean, scale*cov)
+    if rescale:
+        Zn_min = np.min(Zn)
+        Zn_range = np.ptp(Zn)
+        shift = Zn_min - good_min
+        Zn = Zn - shift
+        good_scale = good_range/Zn_range
+        Zn = good_scale*Zn
     Z = Z + pi[n]*multivariate_normal.pdf(pos, mean, scale*cov)
 # normalise
 Z = Z/np.sum(Z)
@@ -49,5 +72,5 @@ plt.clf()
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.plot_surface(X, Y, Z, cmap='cool', linewidth=0)
-plt.savefig('3D.png')
+plt.savefig(filename)
 plt.close()
