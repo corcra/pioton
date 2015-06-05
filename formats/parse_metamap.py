b151aa8e16771b53bba2c4a5c6d5373bfc40463c
Stephanie Hyland <steph.hyland@gmail.com>
2015-06-05 19:12:32 -0400

    interactive mode is significantly more interpretable

--- formats/parse_metamap.py
+++ formats/parse_metamap.py
@@ -6,6 +6,10 @@
 
 import re
 import sys
+
+INTERACTIVE = True
+
+# --- some regexes --- #
 utterance_re = re.compile('^utterance\(')
 phrase_re = re.compile('^phrase\(')
 mappings_re = re.compile('^mappings\(')
@@ -14,6 +18,7 @@ EOU_re = re.compile('^\'EOU')
 
 # this is a file of sentences, fed into metamap
 raw_data_path = ''
+
 # --- grab in paths --- #
 # this is the metamap output. YMMV
 #   created by the command:
@@ -36,6 +41,7 @@ def parse_phrase(line):
     Takes a phrase from machine-readable format, parses its mappings, returns
     a string of mapped terms (into CUIs, when possible).
     """
+    wordmap = dict()
     # list of words in the phrase
     phrase = re.sub('[\'\.]','',re.sub('phrase\(','', line).split(',')[0])
     # get the candidates (and most importantly, their numbers)
@@ -49,17 +55,21 @@ def parse_phrase(line):
         # there were no mappings for this phrase
         parsed_phrase = phrase + ' '
     else:
-        wordmap = dict()
         # accounted for by other words
         delwords = []
         parsed_phrase = ''
         # split the mappings up into 'ev's
         split_mappings = mappings.split('ev(')
+        outstring = ''
         for mapping in split_mappings[1:]:
             CUI = mapping.split(',')[1].strip('\'')
             words = re.split('[\[\]]',','.join(mapping.split(',')[4:]))[1].split(',')
-            print mapping
-            print CUI, words
+            umls_strings = mapping.split(',')[2:4]
+            if INTERACTIVE:
+                outstring += '\n\tAssociation between '+ CUI + ' and ' + ', '.join(map(lambda x: '"'+x+'"',words))
+                if len(words) > 1:
+                    outstring += ' (subsuming ' + ' '.join(map(lambda x: '"'+x+'"', words[1:])) + ')'
+                outstring += '\n\tbased on UMLS strings ' + ', '.join(umls_strings) +'\n'
             wordmap[words[0]] = CUI
             delwords += words[1:]
         # we all do things we are not proud of
@@ -68,18 +78,20 @@ def parse_phrase(line):
                 # lowercase word, cause it is represented in the prolog that way
                 parsed_phrase += wordmap[word.lower()] + ' '
             except KeyError:
-                if word in delwords:
+                if word.lower() in delwords:
                     continue
                 else:
                     parsed_phrase += word + ' '
-    # yolo
-    print phrase
-    try:
-        print delwords
-    except UnboundLocalError:
-        pass
-    print parsed_phrase
-    eh = raw_input('go on?')
+    if INTERACTIVE:
+        if len(wordmap) > 0:
+            # yolo
+            print '\nMapping phrase:',
+            print phrase, '...'
+            print outstring
+            print 'Mapped:', phrase, ' --->',
+            print parsed_phrase
+            print ''
+            eh = raw_input('')
     return parsed_phrase
 
 
@@ -109,6 +121,7 @@ while True:
     if utterance_re.match(line):
         # we are now in an utterance!
         parsed_utterance = parse_utterance()
+        print 'Parsed utterance:',
         print line,
         print '====>', parsed_utterance
         proc_data.write(parsed_utterance+'\n')
